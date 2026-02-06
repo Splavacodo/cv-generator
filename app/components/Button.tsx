53384d14a07222916592f4f63120abcd8ac0d32f
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-06 16:15:04 -0700

    Added save and cancel button types and modified the type annotation of clickHandler to be more generic to better handle different kinds of clickHandler functions

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -9,20 +9,60 @@ export default function Button(
 {
     buttonType: string
     buttonText: string, 
-    clickHandler: (isEditing: boolean) => void
+    clickHandler: () => void
 }) {
         {
             switch (buttonType) {
                 case "edit":
                     return (
                         <button
-                            className="bg-background flex gap-2 rounded-sm px-[8px] py-[4px] hover:opacity-90 cursor-pointer"
-                            onClick={() => clickHandler(true)}
+                            className="bg-background flex gap-2 rounded-sm px-[12px] py-[4px] hover:opacity-90 cursor-pointer"
+                            onClick={clickHandler}
                         >
                             <Image
                                 className="invert"
                                 alt="pencil outline icon"
                                 src="/pencil-outline.svg"
+                                width={15}
+                                height={15}
+                            />
+                            <div
+                                className="text-foreground text-sm"
+                            >
+                                {buttonText}
+                            </div>
+                        </button>
+                    );
+                case "cancel":
+                    return (
+                        <button
+                            className="bg-red-500 flex gap-2 rounded-sm px-[8px] py-[4px] hover:opacity-90 cursor-pointer"
+                            onClick={clickHandler}
+                        >
+                            <Image
+                                className="invert"
+                                alt="pencil outline icon"
+                                src="/close.svg"
+                                width={15}
+                                height={15}
+                            />
+                            <div
+                                className="text-foreground text-sm"
+                            >
+                                {buttonText}
+                            </div>
+                        </button>
+                    );
+                case "save":
+                    return (
+                        <button
+                            className="bg-green-500 flex gap-2 rounded-sm px-[8px] py-[4px] hover:opacity-90 cursor-pointer"
+                            onClick={clickHandler}
+                        >
+                            <Image
+                                className="invert"
+                                alt="pencil outline icon"
+                                src="/check.svg"
                                 width={15}
                                 height={15}
                             />

4cb9e72bc9db1c66ff99b11ce680b3b832dc5a6b
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-06 16:17:15 -0700

    Modified the styling for the ButtonActions component since it's going to be moved to the top of the Header component rather than next to the name header element

--- app/components/ButtonActions.tsx
+++ app/components/ButtonActions.tsx
@@ -1,6 +1,6 @@
 export default function ButtonActions ({ children }: {children: React.ReactNode}) {
     return (
-        <div className="ml-auto flex justify-center items-center">
+        <div className="flex justify-end items-center gap-2">
             {children}
         </div>
     );

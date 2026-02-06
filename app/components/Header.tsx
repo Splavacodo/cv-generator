5c531919b4dc794e4381aa9c0149a08fc800ae76
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-06 16:18:44 -0700

    Modified structure of header elements to better handle the resizing that occurs when elements are replaced with inputs and created more states to be able to preserve original input values before they are changed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,48 +7,79 @@ import Button from "@/app/components/Button";
 export default function Header() {
     const [isEditing, setEditing] = useState(false);
     const [fullName, setFullName] = useState("");
+    const [originalFullName, setOriginalFullName] = useState("");
 
-    function handleEditingStatus(isEditing: boolean) {
-        setEditing(isEditing);
+    function handleEditingStateChange() {
+        setOriginalFullName(fullName);
+        setEditing(true);
     }
 
     function handleFullNameChange(event: React.ChangeEvent<HTMLInputElement>) {
         setFullName(event.target.value);
     }
 
+    function handleCancel() {
+        setFullName(originalFullName);
+        setEditing(false);
+    }
+
+    function handleSave() {
+        setEditing(false);
+    }
+
     return (
-        <div className="flex items-center py-8 px-4 mb-3">
+        <div className="p-3">
             {
-                isEditing ? (
-                    <div className="ml-auto relative w-1/3">
-                        <input 
-                            type="text"
-                            id="full-name"
-                            className="peer w-full border-b-2 border-gray-400 bg-transparent px-2 pb-2 pt-4 text-base outline-none transition-colors focus:border-blue-600"
-                            placeholder=" "
-                            onChange={handleFullNameChange}
-                            value={fullName}
+                isEditing ? 
+                    <ButtonActions>
+                        <Button
+                            buttonType="save"
+                            buttonText="Save"
+                            clickHandler={handleSave}
                         />
-                        <label 
-                            htmlFor="full-name"
-                            className="absolute left-2 -top-1 text-sm text-gray-500 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-base peer-focus:-top-1 peer-focus:text-sm peer-focus:text-blue-600"
-                        >
-                            Full name
-                        </label>
-                    </div>
-                ) : (
-                    <h1 className="ml-auto text-2xl font-medium">
-                        {fullName ? fullName : "Jane Doe"}
-                    </h1>
-                )
+
+                        <Button
+                            buttonType="cancel"
+                            buttonText="Cancel"
+                            clickHandler={handleCancel}
+                        />
+                    </ButtonActions>
+                : 
+                    <ButtonActions>
+                            <Button
+                                buttonType="edit"
+                                buttonText="Edit"
+                                clickHandler={handleEditingStateChange}
+                            />
+                    </ButtonActions>
             }
-            <ButtonActions>
-                <Button
-                    buttonType="edit"
-                    buttonText="Edit"
-                    clickHandler={handleEditingStatus}
-                />
-            </ButtonActions>
+
+            <div className="flex justify-center items-center">
+                {
+                    isEditing ? (
+                        <div className="relative w-1/3">
+                            <input 
+                                type="text"
+                                id="full-name"
+                                className="peer w-full border-b-2 border-gray-400 bg-transparent px-2 pb-2 pt-4 text-base outline-none transition-colors focus:border-blue-600"
+                                placeholder=" "
+                                onChange={handleFullNameChange}
+                                value={fullName}
+                            />
+                            <label 
+                                htmlFor="full-name"
+                                className="absolute left-2 -top-1 text-sm text-gray-500 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-base peer-focus:-top-1 peer-focus:text-sm peer-focus:text-blue-600"
+                            >
+                                Full name
+                            </label>
+                        </div>
+                    ) : (
+                        <h1 className="text-2xl font-medium">
+                            {fullName ? fullName : "Jane Doe"}
+                        </h1>
+                    )
+                }         
+            </div>
         </div>
     );
 }

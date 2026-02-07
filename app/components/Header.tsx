3099d86ec4c6a2ec9c0543866c3fd5706acf109b
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-06 17:08:58 -0700

    Added phone number and email inputs with the same state structure as the name input and made small modifications to previous component's styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,11 +6,20 @@ import Button from "@/app/components/Button";
 
 export default function Header() {
     const [isEditing, setEditing] = useState(false);
+
     const [fullName, setFullName] = useState("");
     const [originalFullName, setOriginalFullName] = useState("");
 
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [originalPhoneNumber, setOriginalPhoneNumber] = useState("");
+
+    const [email, setEmail] = useState("");
+    const [originalEmail, setOriginalEmail] = useState("");
+
     function handleEditingStateChange() {
         setOriginalFullName(fullName);
+        setOriginalPhoneNumber(phoneNumber);
+        setOriginalEmail(email);
         setEditing(true);
     }
 
@@ -18,8 +27,18 @@ export default function Header() {
         setFullName(event.target.value);
     }
 
+    function handlePhoneNumberChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setPhoneNumber(event.target.value);
+    }
+
+    function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setEmail(event.target.value);
+    }
+
     function handleCancel() {
         setFullName(originalFullName);
+        setPhoneNumber(originalPhoneNumber);
+        setEmail(originalEmail);
         setEditing(false);
     }
 
@@ -28,7 +47,7 @@ export default function Header() {
     }
 
     return (
-        <div className="p-3">
+        <div className="flex flex-col p-3">
             {
                 isEditing ? 
                     <ButtonActions>
@@ -57,7 +76,7 @@ export default function Header() {
             <div className="flex justify-center items-center">
                 {
                     isEditing ? (
-                        <div className="relative w-1/3">
+                        <div className="relative w-1/3 mb-2">
                             <input 
                                 type="text"
                                 id="full-name"
@@ -78,7 +97,59 @@ export default function Header() {
                             {fullName ? fullName : "Jane Doe"}
                         </h1>
                     )
-                }         
+                }             
+            </div>
+
+            <div className="flex justify-center items-center">
+                {
+                    isEditing ?
+                        <div className="relative w-1/4">
+                                <input 
+                                    type="text"
+                                    id="phone-number"
+                                    className="peer w-full border-b-2 border-gray-400 bg-transparent px-2 pb-2 pt-4 text-base outline-none transition-colors focus:border-blue-600"
+                                    placeholder=" "
+                                    onChange={handlePhoneNumberChange}
+                                    value={phoneNumber}
+                                />
+                                <label 
+                                    htmlFor="phone-number"
+                                    className="absolute left-2 -top-1 text-sm text-gray-500 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-sm peer-focus:-top-1 peer-focus:text-sm peer-focus:text-blue-600"
+                                >
+                                    Phone number
+                                </label>
+                        </div>
+                    : 
+                        <div className="text-sm">
+                            {phoneNumber ? phoneNumber : "808-808-8080"}
+                        </div>
+                }
+
+                <div className="mx-2">|</div>
+
+                {
+                    isEditing ?
+                        <div className="relative w-1/4">
+                                <input 
+                                    type="email"
+                                    id="user-email"
+                                    className="peer w-full border-b-2 border-gray-400 bg-transparent px-2 pb-2 pt-4 text-base outline-none transition-colors focus:border-blue-600"
+                                    placeholder=" "
+                                    onChange={handleEmailChange}
+                                    value={email}
+                                />
+                                <label 
+                                    htmlFor="user-email"
+                                    className="absolute left-2 -top-1 text-sm text-gray-500 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-sm peer-focus:-top-1 peer-focus:text-sm peer-focus:text-blue-600"
+                                >
+                                    Email
+                                </label>
+                        </div>
+                    :
+                    <div className="text-sm">
+                        {email ? email : "hehe@xd.com"}
+                    </div>
+                }
             </div>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/app/components/Header";
 export default function Home() {
   return (
     <div className="flex justify-center font-sans">
-      <main className="flex min-h-screen w-full max-w-4xl flex-col bg-inherit py-4 overflow-auto">
+      <main className="min-h-screen w-full max-w-4xl bg-inherit py-4 overflow-auto">
         <CVPage>
           <Header></Header>
         </CVPage>

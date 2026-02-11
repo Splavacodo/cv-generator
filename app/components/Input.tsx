db51bb60055f992f5d3c12a6eb45713d6a011556
Splavacodo <52217987+Splavacodo@users.noreply.github.com>
2026-02-10 20:22:20 -0700

    Abstracted repeated input-label pairs into an Input componenet and replaced the pairs with the new component in the Section component

---
+++ app/components/Input.tsx
@@ -0,0 +1,47 @@
+export default function Input(
+    { inputId, inputType, inputPlaceholder, inputValue, onChangeHandler} : 
+    { inputId: string, inputType: string, inputPlaceholder: string, inputValue: string, onChangeHandler: () => void}) {
+
+        {
+            if (inputType === "text") {
+                return (
+                    <div className="relative w-1/3 mb-2">
+                        <input 
+                            type="text"
+                            id={inputId}
+                            className="peer w-full border-b-2 border-gray-400 bg-transparent px-2 pb-2 pt-4 text-sm outline-none transition-colors focus:border-blue-600"
+                            placeholder=" "
+                            onChange={onChangeHandler}
+                            value={inputValue}
+                        />
+                        <label 
+                            htmlFor={inputId}
+                            className="absolute left-2 -top-1 text-sm text-gray-500 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-sm peer-focus:-top-1 peer-focus:text-sm peer-focus:text-blue-600"
+                        >
+                            {inputPlaceholder}
+                        </label>
+                    </div>
+                );
+            }
+            else if (inputType === "email") {
+                return (
+                    <div className="relative w-1/3 mb-2">
+                        <input 
+                            type="email"
+                            id={inputId}
+                            className="peer w-full border-b-2 border-gray-400 bg-transparent px-2 pb-2 pt-4 text-sm outline-none transition-colors focus:border-blue-600"
+                            placeholder=" "
+                            onChange={onChangeHandler}
+                            value={inputValue}
+                        />
+                        <label 
+                            htmlFor={inputId}
+                            className="absolute left-2 -top-1 text-sm text-gray-500 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-sm peer-focus:-top-1 peer-focus:text-sm peer-focus:text-blue-600"
+                        >
+                            {inputPlaceholder}
+                        </label>
+                    </div>
+                );
+            }
+        }
+}
--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import Button from "./Button";
 import ButtonActions from "./ButtonActions";
 import SectionContentList from "./SectionContentList";
+import Input from "./Input";
 
 export default function Section({ title, type }: { title: string, type: string}) {
     const [isAdding, setAdding] = useState(false);
@@ -27,69 +28,39 @@ export default function Section({ title, type }: { title: string, type: string})
                         {isAdding && (
                             <div className="flex flex-col pb-4 gap-2">
                                 <div className="flex justify-between">
-                                    <div className="relative w-1/3 mb-2">
-                                        <input
-                                            id="job-title"
-                                            type="text"
-                                            className="peer w-full border-b-2 border-gray-400 bg-transparent px-2 pb-2 pt-4 text-sm outline-none transition-colors focus:border-blue-600"
-                                            placeholder=" "
-                                        />
-                                        <label
-                                            htmlFor="job-title"
-                                            className="absolute left-2 -top-1 text-sm text-gray-500 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-sm peer-focus:-top-1 peer-focus:text-sm peer-focus:text-blue-600"
-                                        >
-                                            Job title
-                                        </label>
-                                    </div>
+                                    <Input 
+                                        inputId="job-title"
+                                        inputType="text"
+                                        inputPlaceholder="Job title"
+                                        onChangeHandler={() => {}}
+                                        inputValue=""
+                                    />
 
-                                    <div className="relative w-1/3 mb-2">
-                                        <input
-                                            id="dates-of-attendence"
-                                            type="text"
-                                            className="peer w-full border-b-2 border-gray-400 bg-transparent px-2 pb-2 pt-4 text-sm outline-none transition-colors focus:border-blue-600"
-                                            placeholder=" "
-                                        />
-
-                                        <label
-                                            htmlFor="dates-of-attendence"
-                                            className="absolute left-2 -top-1 text-sm text-gray-500 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-sm peer-focus:-top-1 peer-focus:text-sm peer-focus:text-blue-600"
-                                        >
-                                            Start date - End date
-                                        </label>
-                                    </div>
+                                    <Input 
+                                        inputId="dates-of-attendence"
+                                        inputType="text"
+                                        inputPlaceholder="Start date - End date"
+                                        onChangeHandler={() => {}}
+                                        inputValue=""
+                                    />
                                 </div>
 
                                 <div className="flex justify-between">
-                                    <div className="relative w-1/3 mb-2">
-                                        <input
-                                            id="company-name"
-                                            type="text"
-                                            className="peer w-full border-b-2 border-gray-400 bg-transparent px-2 pb-2 pt-4 text-sm outline-none transition-colors focus:border-blue-600"
-                                            placeholder=" "
-                                        />
-                                        <label
-                                            htmlFor="company-name"
-                                            className="absolute left-2 -top-1 text-sm text-gray-500 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-sm peer-focus:-top-1 peer-focus:text-sm peer-focus:text-blue-600"
-                                        >
-                                            Company name
-                                        </label>
-                                    </div>
+                                    <Input 
+                                        inputId="company-name"
+                                        inputType="text"
+                                        inputPlaceholder="Company name"
+                                        onChangeHandler={() => {}}
+                                        inputValue=""
+                                    />
 
-                                    <div className="relative w-1/3 mb-2">
-                                        <input
-                                            id="location"
-                                            type="text"
-                                            className="peer w-full border-b-2 border-gray-400 bg-transparent px-2 pb-2 pt-4 text-sm outline-none transition-colors focus:border-blue-600"
-                                            placeholder=" "
-                                        />
-
-                                        <label
-                                            htmlFor="location"
-                                            className="absolute left-2 -top-1 text-sm text-gray-500 transition-all peer-placeholder-shown:top-5 peer-placeholder-shown:text-sm peer-focus:-top-1 peer-focus:text-sm peer-focus:text-blue-600"
-                                        >
-                                            Location
-                                        </label>
-                                    </div>
+                                    <Input 
+                                        inputId="job-location"
+                                        inputType="text"
+                                        inputPlaceholder="Job location"
+                                        onChangeHandler={() => {}}
+                                        inputValue=""
+                                    />
                                 </div>
 
                                 <textarea 
